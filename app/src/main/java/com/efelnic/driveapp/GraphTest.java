21347463d4aa0cfb50c187c94aff7b074ded9078
riccar2 <roberto.riccardi.08@gmail.com>
2016-03-25 22:09:58 -0400

    Example of gps setting on/off as settiing for graph under graphTest activity

--- app/src/main/java/com/efelnic/driveapp/GraphTest.java
+++ app/src/main/java/com/efelnic/driveapp/GraphTest.java
@@ -1,6 +1,8 @@
 package com.efelnic.driveapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,32 +21,39 @@ public class GraphTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_test);
 
-        ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(4f, 0));
-        entries.add(new Entry(8f, 1));
-        entries.add(new Entry(6f, 2));
-        entries.add(new Entry(12f, 3));
-        entries.add(new Entry(18f, 4));
-        entries.add(new Entry(9f, 5));
-        RadarDataSet dataSet = new RadarDataSet(entries, "# of Calls");
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Boolean a = sp.getBoolean("prefGps", false);
 
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
+        if (a == true){
+            ArrayList<Entry> entries = new ArrayList<>();
+            entries.add(new Entry(4f, 0));
+            entries.add(new Entry(8f, 1));
+            entries.add(new Entry(6f, 2));
+            entries.add(new Entry(12f, 3));
+            entries.add(new Entry(18f, 4));
+            entries.add(new Entry(9f, 5));
+            RadarDataSet dataSet = new RadarDataSet(entries, "# of Calls");
+
+            ArrayList<String> labels = new ArrayList<String>();
+            labels.add("January");
+            labels.add("February");
+            labels.add("March");
+            labels.add("April");
+            labels.add("May");
+            labels.add("June");
 
 
-        Context context = this;
+            Context context = this;
 
-        RadarChart chart = new RadarChart(context);
-        setContentView(chart);
-        RadarData data = new RadarData(labels, dataSet);
-        chart.setData(data);
-        chart.setDescription("# of times Alice called Bob");
-
+            RadarChart chart = new RadarChart(context);
+            setContentView(chart);
+            RadarData data = new RadarData(labels, dataSet);
+            chart.setData(data);
+            chart.setDescription("# of times Alice called Bob");
+        }
+        else{
+            //
+        }
 
     }
 
